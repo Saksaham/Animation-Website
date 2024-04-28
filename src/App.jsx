130e408389888f0909ae697972e179b4fed5280d
Saksham Khurana <83787870+Saksaham@users.noreply.github.com>
2024-04-28 18:56:11 +0530

    Upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import LandingPage from './components/LandingPage'
+import Marquee from './components/Marquee'
 
 function App() {
   return (
-    <div className = 'bg-sky-600'>App</div>
+    <div className = 'w-full min-h-screen text-white bg-zinc-900'>
+      <Navbar/>
+      <LandingPage/>
+      <Marquee/>
+    </div>
   )
 }
 
