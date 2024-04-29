45275dba74e91eb72a0c30e11a2ee71dafa0cc2e
Saksham Khurana <83787870+Saksaham@users.noreply.github.com>
2024-04-30 03:01:30 +0530

    upload

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Featured() {
   return (
     <div className='w-full py-20'>
-        <div className='w-full px-20 border-b-[1px] border-zinc-700 pb-15'>
+        <div className='w-full px-20 border-b-[1px] border-zinc-700 pb-20'>
             <h1 className='text-6xl font-["Neue_Montreal"] tracking-tight'>Featured projects</h1>
         </div>
     </div>
