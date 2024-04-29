0fb5d1afc2c9f12055d259ba223dc9ebe8461e5d
Saksham Khurana <83787870+Saksaham@users.noreply.github.com>
2024-04-30 01:38:06 +0530

    upload

---
+++ src/components/Featured.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className='w-full py-20'>
+        <div className='w-full px-16 border-bg-green'>
+            <h1 className='text-6xl font-["Neue_Montreal"]'>Featured projects</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
